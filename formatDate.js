c13d926e3474fab62ec23912a8163c4e023de54a
mpbusinesscash <mpbusinesscash@gmail.com>
2025-04-26 14:54:41 +0000

    Posting in wikiverse

---
+++ formatDate.js
@@ -0,0 +1,15 @@
+export function formatDate(dateString) {
+    console.log(dateString)
+    const date = new Date(dateString);
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const seconds = date.getUTCSeconds();
+    const month = date.getUTCMonth();
+    const day = date.getUTCDate();
+    const year = date.getUTCFullYear();
+  
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const formattedDate = `${monthNames[month]} ${day} ${year}, ${hours}:${minutes.toString().padStart(2, '0')}:${seconds} UTC`;
+    return formattedDate;
+}  
