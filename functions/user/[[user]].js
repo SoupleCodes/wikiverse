cabbde28c2c223d0dfb22281c1deac8ab14928f9
mpbusinesscash <mpbusinesscash@gmail.com>
2025-08-17 20:23:06 +0000

    d

--- functions/user/[[user]].js
+++ functions/user/[[user]].js
@@ -1,4 +1,7 @@
 export function onRequest(context) {
     console.log("catchall:", context.params.catchall);
-    return new Response(context.params.catchall);
+    return new Response(
+      `<a href="/">Hello, ` + context.params.catchall + `!</a>`, 
+      { headers: { 'Content-Type': 'text/html'} }
+  );
 }
