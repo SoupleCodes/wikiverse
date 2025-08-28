9b300255acafdaa735f7aa0c9de5cad2c016746e
mpbusinesscash <mpbusinesscash@gmail.com>
2025-08-28 21:45:11 +0000

    more testing *grr*

--- functions/user/[[user]].js
+++ functions/user/[[user]].js
@@ -1,10 +1,11 @@
 export async function onRequest(context) {
-    const { request, env } = context
+  const { request, env } = context;
 
-    const response = await env.ASSETS.fetch('/user/index.html')
-    const content = await response.text()
-    
-    return new Response(
-      content, response
+  const response = await env.ASSETS.fetch('/user/index.html');
+  const htmlContent = await response.text();
+
+  return new Response(
+    htmlContent,
+    { headers: { 'Content-Type': 'text/html'} }
   );
 }
