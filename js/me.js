b458422cb2ac1c50ce2612565134a198e7515251
mpbusinesscash <mpbusinesscash@gmail.com>
2025-09-04 20:43:47 +0000

    test preview

--- js/me.js
+++ js/me.js
@@ -1,6 +1,7 @@
 const token = localStorage.token
 let u = localStorage.user
 
+const previewIframeEl = document.querySelector('#preview-window iframe').contentWindow.document
 const displayInputEl = document.querySelector('input[name="display_name"]')
 const pfpUrlEl = document.querySelector('input[name="pfp_url"]')
 const bannerUrlEl = document.querySelector('input[name="banner_url"]')
@@ -74,21 +75,18 @@ function addLink(data) {
     const userLabel = document.createElement('label')
     const linkUserInput = document.createElement('input')
     linkUserInput.name = 'link-user'
-    linkUserInput.placeholder = 'John Doe'
     linkUserInput.value = data.username
     userLabel.appendChild(linkUserInput)
 
     const siteNameLabel = document.createElement('label')
     const siteNameInput = document.createElement('input')
     siteNameInput.name = 'link-sitename'
-    siteNameInput.placeholder = 'Wikiverse'
     siteNameInput.value = data.name
     siteNameLabel.appendChild(siteNameInput)
 
     const urlLabel = document.createElement('label')
     const urlInput = document.createElement('input')
     urlInput.name = 'link-sitename'
-    urlInput.placeholder = 'https://wikiverse.pages.dev'
     urlInput.value = data.url
     urlLabel.appendChild(urlInput)
 
@@ -128,3 +126,139 @@ if (linksEl) {
         ))
     })
 }
+
+const content = previewIframeEl.createElement('div'); content.id = 'content'
+const userStyle = previewIframeEl.createElement('style'); userStyle.id = 'user-style'
+
+function updateStyle() { userStyle.innerHTML = styleEl.value }
+
+const header = previewIframeEl.createElement('div'); header.id = 'header';
+const headerDisplay = previewIframeEl.createElement('h5'); headerDisplay.id = 'display';
+if (u.username.endsWith('s')) {
+    headerDisplay.textContent= u.username + "' profile"
+} else {
+    headerDisplay.textContent= u.username + "'s profile"
+}
+headerDisplay.title = 'h5#display'
+header.appendChild(headerDisplay)
+
+const banner = previewIframeEl.createElement('div'); banner.id = 'banner';
+if (u.banner_url) {
+    const img = previewIframeEl.createElement('img')
+    img.src = u.banner_url
+    banner.appendChild(img)
+}
+banner.title = '#banner img'
+function updateBanner() { banner.querySelector('img').src = bannerUrlEl.value }
+
+const side1 = previewIframeEl.createElement('div'); side1.id = 'side1';
+    const avatarDiv = previewIframeEl.createElement('div'); avatarDiv.id = 'avatar'
+    const img = previewIframeEl.createElement('img'); img.src = u.pfp_url || '/images/ui/default.png'; 
+    img.title = '#avatar img'
+    avatarDiv.appendChild(img); side1.appendChild(avatarDiv)
+    const h5display = previewIframeEl.createElement('h5'); h5display.id = 'display_name'; h5display.textContent = u.display_name || ''
+    h5display.title = 'h5#display_name'
+    side1.appendChild(h5display)
+
+    const statsEl = previewIframeEl.createElement('div'); statsEl.id = 'stats'; statsEl.title = '#stats'
+        const ustatsM = previewIframeEl.createElement('h6'); ustatsM.textContent = 'Member since:'
+        const ujoinDateEl = previewIframeEl.createElement('small'); ujoinDateEl.id = 'join-date'; ujoinDateEl.textContent = returnUTCTime(u.created_at)
+        const ustatsEl = previewIframeEl.createElement('h6'); ustatsEl.textContent = 'Last seen:'
+        const ulastSeenEl = previewIframeEl.createElement('small'); ulastSeenEl.id = 'last-seen'; ulastSeenEl.textContent = returnUTCTime(u.last_activity)
+        const uLocationEl = previewIframeEl.createElement('h6'); uLocationEl.textContent = 'Location:'
+        const uLocationSmallEl = previewIframeEl.createElement('small'); uLocationSmallEl.id = 'user-location'; uLocationSmallEl.textContent = u.location
+    statsEl.appendChild(ustatsM)
+    statsEl.appendChild(ujoinDateEl)
+    statsEl.appendChild(ustatsEl)
+    statsEl.appendChild(ulastSeenEl)
+    statsEl.appendChild(uLocationEl)
+    statsEl.appendChild(uLocationSmallEl)
+side1.appendChild(statsEl)
+
+function updateDisplayName() { h5display.textContent = displayInputEl.value }
+function updateLocation() { uLocationSmallEl.textContent = locationEl.value }
+function updatePfp() { avatarDiv.querySelector('img').src = pfpUrlEl.value }
+
+const side2 = previewIframeEl.createElement('div'); side2.id = 'side2'
+function makeGroup(title, contentID, contentElType) {
+    const group = previewIframeEl.createElement('div');
+    group.classList.add('group')
+    
+    const titleEl = previewIframeEl.createElement('h5');
+    titleEl.classList.add('title')
+    titleEl.textContent = title
+    group.appendChild(titleEl)
+
+    const contentEl = previewIframeEl.createElement(contentElType || 'ul');
+    contentEl.id = contentID
+    contentEl.title = contentElType + '#' + (contentID || 'ul')
+    group.appendChild(contentEl)
+
+    side2.appendChild(group)
+    return contentEl
+}
+const uAboutMeEl = makeGroup('About me:', 'aboutme', 'p')
+uAboutMeEl.innerHTML = bbcodeparse(u.about_me)
+function updateAboutMe() { uAboutMeEl.innerHTML = bbcodeparse(aboutMeEl.value) }
+
+const urecentlyEl = makeGroup('Recently edited articles:', 'recent-articles')
+urecentlyEl.innerHTML = `
+    <li><a href="#">Article</a><p>by someone</p></li>
+    <li><a href="#">Article</a><p>by someone</p></li>
+    <li><a href="#">Article</a><p>by someone</p></li>
+    <li><a href="#">Article</a><p>by someone</p></li>
+`
+const urecentlyCommentsEl = makeGroup('Recent comments:', 'recent-comments')
+urecentlyCommentsEl.innerHTML = `
+    <li><a href="#">souple</a><p>on your blog</p></li>
+    <li><a href="#">souple</a><p>on your blog</p></li>
+    <li><a href="#">souple</a><p>on your blog</p></li>
+    <li><a href="#">souple</a><p>on your blog</p></li>
+`
+const archiveEl = makeGroup('Archive:', 'archive', 'table')
+archiveEl.innerHTML = `<tbody><tr><td class="social-name"><a href="/~${u.username}/archive">December 1969</a> (999)</td></tr></tbody>`
+
+const socialLinksEl = makeGroup('Contact me:', 'contactme', 'table')
+Array.from(u.social_links).map((s) => {
+    let trEl = previewIframeEl.createElement('tr')
+
+    let tdEl = previewIframeEl.createElement('td');
+    tdEl.classList.add('social-name')
+    tdEl.width = '25%'
+    tdEl.textContent = s.name
+    
+    let tdEl2 = previewIframeEl.createElement('td');
+    tdEl2.classList.add('social-name')
+    tdEl2.width = '75%'
+
+    let aEl = previewIframeEl.createElement('a');
+    aEl.href = s.url
+    aEl.textContent = s.username
+    tdEl2.appendChild(aEl)
+
+    trEl.appendChild(tdEl)
+    trEl.appendChild(tdEl2)
+    socialLinksEl.appendChild(trEl)
+})
+
+
+const side3 = previewIframeEl.createElement('div'); side3.id = 'side3'
+const side4 = previewIframeEl.createElement('div'); side4.id = 'side4'
+
+previewIframeEl.querySelector('body').appendChild(header); previewIframeEl.querySelector('body').appendChild(banner);
+content.appendChild(side1); content.appendChild(side2);
+content.appendChild(side3); content.appendChild(side4);
+previewIframeEl.querySelector('body').appendChild(content)
+previewIframeEl.querySelector('body').appendChild(userStyle)
+
+var link = document.createElement('link');
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    link.href = '/styles/main.css'
+previewIframeEl.querySelector('head').appendChild(link)
+
+var link2 = document.createElement('link');
+    link2.type = 'text/css';
+    link2.rel = 'stylesheet';
+    link2.href = '/styles/user.css'
+previewIframeEl.querySelector('head').appendChild(link2)
--- js/user.js
+++ js/user.js
@@ -1,7 +1,6 @@
 let test = window.location.hostname == 'wikiverse.pages.dev' ? window.location.pathname.slice(2) : null
 const user = (test || new URLSearchParams(window.location.search).get(""))
 
-function returnUTCTime(t) { return new Date(t).toLocaleDateString('UTC') }
 function createBlog(data) {
     const blog = document.createElement('div')
     blog.classList.add('weblog')
