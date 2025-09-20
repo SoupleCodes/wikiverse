444ab0755d6fade26b59844a17b617d1a8ec6fd2
mpbusinesscash <mpbusinesscash@gmail.com>
2025-09-20 17:07:17 +0000

    Forgot to close the parentheses

--- js/create.js
+++ js/create.js
@@ -242,7 +242,7 @@ async function submitBlog() {
   let style = document.querySelector('#edit-style textarea').value
   let tags = []
   let music = {}
-  if (musicToggl.checked {
+  if (musicToggl.checked) {
     const detailOptions = document.querySelector('#tabs-music.tab-content').children
 
     let artist_name = detailOptions[0].value
