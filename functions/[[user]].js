f46ea3f44969e2c7ba63952157b7e0d087ddc769
mpbusinesscash <mpbusinesscash@gmail.com>
2025-09-29 22:08:57 +0000

    hmm

--- functions/[[user]].js
+++ functions/[[user]].js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
     const { request, env } = context;
-    const segments = context.params.catchall.user
+    const segments = context.params.user
     const user = segments[0]
     const page = segments[1]
 
